e7ca161cae87638b2a56296189c154eb2663d747
justinmichaels006 <justin@couchbase.com>
2017-02-18 14:22:53 -0800

    Compress Doc Example

--- src/main/java/com/couchbase/CompressDoc.java
+++ src/main/java/com/couchbase/CompressDoc.java
@@ -1,23 +1,16 @@
 package com.couchbase;
 
-import com.couchbase.client.core.BackpressureException;
-import com.couchbase.client.core.time.Delay;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.document.JsonDocument;
 import com.couchbase.client.java.document.LegacyDocument;
-import com.couchbase.client.java.error.TemporaryFailureException;
 import com.couchbase.client.java.transcoder.LegacyTranscoder;
 import com.couchbase.client.java.view.ViewQuery;
 import com.couchbase.client.java.view.ViewResult;
 import com.couchbase.client.java.view.ViewRow;
-import rx.Observable;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
-
-import static com.couchbase.client.java.util.retry.RetryBuilder.anyOf;
 
 public class CompressDoc {
 
@@ -26,7 +19,7 @@ public class CompressDoc {
         Bucket myB = ConnectionManager.getConnection();
         Bucket myB2 = ConnectionManager.getConnection2();
 
-        LegacyTranscoder lTrans = new LegacyTranscoder(0);
+        LegacyTranscoder lTrans = new LegacyTranscoder(1);
         List<LegacyDocument> docArray = new ArrayList<>();
         final int MAX_RETRIES = 20000;
         final int RETRY_DELAY = 50;
@@ -34,29 +27,31 @@ public class CompressDoc {
         int i = 0;
         //String abeer = "21st_amendment_brewery_cafe-21a_ipa";
 
-        ViewResult result = myB.query(ViewQuery.from("beer", "allkey"));
+        ViewResult result = myB.query(ViewQuery.from("beer", "allkeys"));
+        //N1qlQueryResult result2 = myB.query((N1qlQuery.simple("select meta(`beer-sample`).id from `beer-sample`")));
+
         for (ViewRow row : result) {
+        //for (N1qlQueryRow row : result2) {
+
             // Create an id to use
             UUID newID = UUID.randomUUID();
 
-            //TODO: Add query to read all docs
+            //JsonDocument jsonDoc = myB.get(row.toString());
             JsonDocument jsonDoc = myB.get(row.id());
-            //JsonObject jsonObj = jsonDoc.content();
 
             LegacyDocument ldoc = LegacyDocument.create(newID.toString(), jsonDoc);
             Object newdoc = lTrans.encode(ldoc);
-            LegacyDocument ldoc2 = LegacyDocument.create(newID.toString(), newdoc);
+            LegacyDocument ldoc2 = LegacyDocument.create(newID.toString(), newdoc.toString());
 
             // Add other logic here (TODO faker)
-            // Build the array of items to load (TODO Batching)
+            myB2.upsert(ldoc2);
 
-            docArray.add(i, ldoc2);
-            i = i + 1;
+            // Build the array of items to load (TODO Batching)
+            /*docArray.add(i, ldoc2);
+            i = i + 1;*/
         }
 
-        //myB.async().upsert(ldoc2);
-
-        Observable
+        /*Observable
                 .from(docArray)
                 .flatMap(doc -> {
                     return myB2.async().upsert(doc)
@@ -67,10 +62,11 @@ public class CompressDoc {
                             .retryWhen(anyOf(TemporaryFailureException.class)
                                     .max(MAX_RETRIES)
                                     .delay(Delay.exponential(TimeUnit.MILLISECONDS, RETRY_DELAY, MAX_DELAY)).build());
-                }).toBlocking().subscribe(document1 -> {});
+                }).toBlocking().subscribe(document1 -> {});*/
 
         System.out.println("compressed beer sample");
         myB.close();
+        myB2.close();
 
     }
 }
