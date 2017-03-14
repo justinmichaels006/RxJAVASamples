cf395c5e260e9dca88d41c2eb5a6e51a26bdff91
justinmichaels006 <justin@couchbase.com>
2017-03-14 05:18:48 -0700

    Update subdoc example

--- src/main/java/com/couchbase/SampleSub.java
+++ src/main/java/com/couchbase/SampleSub.java
@@ -3,52 +3,36 @@ package com.couchbase;
 
 import com.couchbase.client.core.message.kv.subdoc.multi.Lookup;
 import com.couchbase.client.core.message.kv.subdoc.multi.Mutation;
-import com.couchbase.client.core.retry.FailFastRetryStrategy;
 import com.couchbase.client.java.Bucket;
-import com.couchbase.client.java.CouchbaseCluster;
-import com.couchbase.client.java.env.CouchbaseEnvironment;
-import com.couchbase.client.java.env.DefaultCouchbaseEnvironment;
 import com.couchbase.client.java.subdoc.DocumentFragment;
 import com.couchbase.client.java.subdoc.MutateInBuilder;
 
 public class SampleSub {
 
-    public static void main(String[] args) throws Exception {
-
-        CouchbaseEnvironment env = DefaultCouchbaseEnvironment.builder()
-                .dnsSrvEnabled(false)
-                .kvEndpoints(4) //if you have batch upload can gain throughput
-                // but with small operations can cause contention with socket overhead
-                .computationPoolSize(4) // very rare needed to be changed
-                .queryEndpoints(4) // long running N1QL queries
-                //.observeIntervalDelay()
-                .retryStrategy(FailFastRetryStrategy.INSTANCE) // only needed in demanding cache-only use case
-                .build();
-        CouchbaseCluster cluster = CouchbaseCluster.create(env, "192.168.61.101");
-        Bucket bucket = cluster.openBucket("testload");
+    public String SubDocModification(Bucket bucket, String aKey) throws Exception {
 
         System.out.println("Lookup attempt");
 
         DocumentFragment<Lookup> lookupRslt = bucket
                 .lookupIn("Int02")
-                .get("parent.name")
+                .get("parent")
                 .execute();
 
-        String subValue = lookupRslt.content("parent.name", String.class);
+        String subValue = lookupRslt.content("parent", String.class);
         System.out.println("Lookup result" + subValue);
 
         System.out.println("Mutate attempt");
-
+        
         MutateInBuilder builder = bucket.mutateIn("Int02");
-        DocumentFragment<Mutation> result = builder.replace("parent.age", 40).execute();
+        DocumentFragment<Mutation> result = builder.replace("parent", 40).execute();
 
         DocumentFragment<Lookup> result2 = bucket
                 .lookupIn("Int02")
-                .get("parent.age")
+                .get("parent")
                 .execute();
         System.out.println("Mutation result" + result2);
 
-        bucket.close();
-        cluster.disconnect();
+        return result.toString();
+
     }
 }
