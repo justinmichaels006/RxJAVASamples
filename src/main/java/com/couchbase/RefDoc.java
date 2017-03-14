5f36052049026debaf3a2d2f7423876b67470492
justinmichaels006 <justin@couchbase.com>
2017-03-14 05:40:38 -0700

    Update refdoc example

--- src/main/java/com/couchbase/RefDoc.java
+++ src/main/java/com/couchbase/RefDoc.java
@@ -6,27 +6,15 @@ import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.transcoder.JsonTranscoder;
 import rx.Observable;
 
-import java.util.UUID;
-
-/**
- * Created by justin on 1/20/17.
- */
 public class RefDoc {
 
-    public static void main(String[] args) {
-        //public BinaryDocument saveBinary (String bucketName, BinaryDocument document, Long expiry, TimeUnit timeUnit){
-        //ConnectionManager connectionManager = ConnectionManager.getConnection();
+    public JsonObject addRefDoc(Bucket bucket) {
 
-        Bucket bucket = ConnectionManager.getConnection();
-        UUID newID = UUID.randomUUID();
         String abeer = "21st_amendment_brewery_cafe-21a_ipa";
         String abrew = "21st_amendment_brewery_cafe";
         JsonDocument jdoc = null;
         JsonObject jobj = null;
-        //String jREFDOC = null;
         JsonTranscoder trans = new JsonTranscoder();
-
-        //JsonDocument theBrewery = null;
 
         Observable theBrewery = bucket.async().get(abeer)
                 .map(doc -> doc.content().getString("brewery_id"))
@@ -47,20 +35,14 @@ public class RefDoc {
         } catch (Exception e) {e.printStackTrace();}
 
         JsonObject finalJobj = jobj;
-        /*Observable<JsonDocument> siteREFDOC = Observable
-                .just(jREFDOC)
-                .map(s -> JsonDocument.create(s, finalJobj))
-                .flatMap(doc -> bucket.async().upsert(doc));*/
 
         try {
             jREFDOC.toBlocking()
                     .subscribe(theSite -> JsonDocument.create(theSite.toString(), finalJobj));
-                    //.flatMap(doc -> bucket.async().upsert(doc));
-                            //System.out.println("Web REFDOC " + theSite));
         } catch (Exception e) {
             System.out.println("Error during web doc " + e);
         }
 
-        bucket.close();
+        return finalJobj;
     }
 }
