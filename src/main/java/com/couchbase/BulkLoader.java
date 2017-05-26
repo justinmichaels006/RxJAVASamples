3ab81b73ca5c90b61ca9392a641fc6c870f8819b
justinmichaels006 <justin@couchbase.com>
2017-05-26 06:50:50 -0700

    Update bulk example

--- src/main/java/com/couchbase/BulkLoader.java
+++ src/main/java/com/couchbase/BulkLoader.java
@@ -2,72 +2,25 @@ package com.couchbase;
 
 import com.couchbase.client.core.BackpressureException;
 import com.couchbase.client.core.time.Delay;
-import com.couchbase.client.deps.io.netty.channel.ConnectTimeoutException;
 import com.couchbase.client.java.Bucket;
 import com.couchbase.client.java.document.JsonDocument;
-import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.error.TemporaryFailureException;
-import com.couchbase.client.java.transcoder.JsonTranscoder;
-import org.json.simple.parser.JSONParser;
 import rx.Observable;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import static com.couchbase.client.java.util.retry.RetryBuilder.anyOf;
 
 public class BulkLoader {
 
-    public String bLoad(Bucket bucket) throws IOException, org.json.simple.parser.ParseException {
+    public static String bLoad(Bucket bucket, List<JsonDocument> docArray) {
 
-        int numDocs = 20000;
-        long start = System.nanoTime();
         final int MAX_RETRIES = 20000;
         final int RETRY_DELAY = 50;
         final int MAX_DELAY = 1000;
 
-        final String filePath1 = "/path/to/file.json";
-        FileReader reader1 = null;
-
-        try {
-            reader1 = new FileReader(filePath1);
-        } catch (FileNotFoundException ex) {
-            //ex.printStackTrace();
-            ex.getMessage();
-        }
-
-        JSONParser jsonParser = new JSONParser();
-        JsonTranscoder trans = new JsonTranscoder();
-        List<JsonDocument> docArray = new ArrayList<>();
-        JsonObject jsonObj = null;
-        JsonDocument jsonDoc = null;
-
-        String jsonString1 = jsonParser.parse(reader1).toString();
-
-        for (int i = 0; i < numDocs; i++) {
-
-            try {
-                jsonObj = trans.stringToJsonObject(jsonString1);
-            } catch (Exception e) {e.printStackTrace();}
-
-            // Create an id to use
-            UUID theID = UUID.randomUUID();
-
-            // Add the ID as an attribute to the document
-            jsonObj.put("THIS_ID", i + "::" + theID.toString());
-            // Add other logic here (TODO faker)
-            // Create the json document
-            jsonDoc = JsonDocument.create(theID.toString(), jsonObj);
-            // Build the array of items to load
-            // TODO Batching
-            docArray.add(i, jsonDoc);
-        }
-
+        long start = System.nanoTime();
         Observable
                 .from(docArray)
                 .flatMap(doc -> {
@@ -77,15 +30,12 @@ public class BulkLoader {
                                     .max(MAX_RETRIES)
                                     .delay(Delay.exponential(TimeUnit.MILLISECONDS, RETRY_DELAY, MAX_DELAY)).build())
                             .retryWhen(anyOf(TemporaryFailureException.class)
-                                    .max(MAX_RETRIES).delay(Delay.exponential(TimeUnit.MILLISECONDS, RETRY_DELAY, MAX_DELAY)).build())
-                            .retryWhen(anyOf(ConnectTimeoutException.class)
-                                    .max(MAX_RETRIES).delay(Delay.exponential(TimeUnit.SECONDS, RETRY_DELAY, MAX_DELAY)).build());
+                                    .max(MAX_RETRIES).delay(Delay.exponential(TimeUnit.MILLISECONDS, RETRY_DELAY, MAX_DELAY)).build());
                 }).toBlocking().subscribe(document1 -> {});
-
         long end = System.nanoTime();
 
-        System.out.println("Bulk loading " + numDocs + " docs took: " + TimeUnit.NANOSECONDS.toSeconds(end - start) + "s.");
-        return ("Bulk loading " + numDocs + " docs took: " + TimeUnit.NANOSECONDS.toSeconds(end - start) + "s.");
+        System.out.println("Bulk loading " + docArray.size() + " docs took: " + TimeUnit.NANOSECONDS.toSeconds(end - start) + "s.");
+        return ("Bulk loading " + docArray.size()  + " docs took: " + TimeUnit.NANOSECONDS.toSeconds(end - start) + "s.");
     }
 
 }
--- src/main/java/com/couchbase/Retry.java
+++ src/main/java/com/couchbase/Retry.java
@@ -1,23 +1,80 @@
 package com.couchbase;
 
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
+import com.couchbase.client.java.transcoder.JsonTranscoder;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.UUID;
 
 public class Retry {
 
     private static final int ThreadCount = 10;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException {
 
         Bucket bucket = ConnectionManager.getConnection();
 
+        int numDocs = 10;
+        int groupDocs = 10;
+        FileReader reader1 = null;
+        JSONParser jsonParser = new JSONParser();
+        JsonTranscoder trans = new JsonTranscoder();
+        List<JsonDocument> docArray = new ArrayList<>();
+        JsonObject jsonObj = null;
+        JsonDocument jsonDoc = null;
+        Random r = new Random();
+        final String filePath1 = "/Users/justin/Documents/javastuff/RxJAVASamples/src/main/java/com/couchbase/file.json";
+        //BufferedReader buff1 = null;
+
+        reader1 = new FileReader(filePath1);
+        String jsonString1 =  jsonParser.parse(reader1).toString();
+
         try {
-            ConcurrentQuery.testParalel(bucket);
+            jsonObj = trans.stringToJsonObject(jsonString1);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        for (int i = 0; i < (numDocs*groupDocs); i=i+groupDocs) {
+            for (int z = 0; z < groupDocs; z++) {
+                jsonObj.put("eqr_xref", i);
+                // Create an id to use
+                UUID uid = UUID.fromString("38400000-8cf0-11bd-b23e-10b96e4ef00d");
+                String theID = uid.randomUUID().toString();
+
+                // Add the ID as an attribute to the document
+                jsonObj.put("reward_id", theID);
+                // Create the json document
+                jsonDoc = JsonDocument.create(theID, jsonObj);
+                System.out.println("reward_id..." + jsonObj.get("reward_id"));
+                System.out.println("eqr_xref..." + jsonObj.get("eqr_xref"));
+                System.out.println(jsonDoc);
+                // Add other logic here (TODO faker)
+                // TODO Batching
+                //bucket.upsert(jsonDoc);
+                docArray.add(i+z, jsonDoc);
+            }
+
+        }
+
+        try {
+            //ConcurrentQuery.testParalel(bucket);
+            BulkLoader.bLoad(bucket, docArray);
         } catch (Exception e)
         {
             System.out.println(e);
         }
 
         // Disconnect and clear all allocated resources
+        System.out.println("debug ... bye");
         bucket.close();
     }
 }
