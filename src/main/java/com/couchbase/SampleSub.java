77810e517c155fc73c32bdecdb01474bf381bdaf
justinmichaels006 <justin@couchbase.com>
2017-07-14 10:26:25 -0700

    Update bulk example

--- src/main/java/com/couchbase/SampleSub.java
+++ src/main/java/com/couchbase/SampleSub.java
@@ -4,6 +4,8 @@ package com.couchbase;
 import com.couchbase.client.core.message.kv.subdoc.multi.Lookup;
 import com.couchbase.client.core.message.kv.subdoc.multi.Mutation;
 import com.couchbase.client.java.Bucket;
+import com.couchbase.client.java.document.JsonDocument;
+import com.couchbase.client.java.document.json.JsonObject;
 import com.couchbase.client.java.subdoc.DocumentFragment;
 import com.couchbase.client.java.subdoc.MutateInBuilder;
 
@@ -16,6 +18,20 @@ public class SampleSub {
         DocumentFragment<Lookup> lookupRslt = bucket
                 .lookupIn("Int02")
                 .get("parent")
+                .execute();
+
+        JsonDocument somedoc = JsonDocument.create("someid",
+                JsonObject.create()
+                        .put("logins", 0)
+                        .put("searches", 0)
+                        .put("path", 50)
+        );
+        bucket.upsert(somedoc);
+
+        bucket.mutateIn("someKey")
+                .counter("user.logins", 1,false)
+                .counter("user.searches", 5, false)
+                .counter("some.other.path", -1, false)
                 .execute();
 
         String subValue = lookupRslt.content("parent", String.class);
